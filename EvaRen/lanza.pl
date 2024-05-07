c71a6d73e9e854b0fd252f196c48e77bbc141bd9
JuanBap <124364006+JuanBap@users.noreply.github.com>
2024-05-06 19:01:01 -0500

    última versión

--- EvaRen/lanza.pl
+++ EvaRen/lanza.pl
@@ -1,31 +1,62 @@
-#!/usr/bin/perl
 #**************************************************************
-#         		Pontificia Universidad Javeriana
+#               Pontificia Universidad Javeriana
 #     Autor: J. Corredor
 #     Fecha: Febrero 2024
 #     Materia: Sistemas Operativos
 #     Tema: Taller de Evaluación de Rendimiento
 #     Fichero: script automatización ejecución por lotes 
-#****************************************************************/
+#****************************************************************
 
-$Path = `pwd`;  # Ejecuta el comando 'pwd' que imprime el directorio de trabajo actual y lo guarda en la variable $Path.
-chomp($Path);   # Elimina cualquier carácter de nueva línea al final de la cadena en $Path.
+#**************************************************************
+#               Pontificia Universidad Javeriana
+#     Autor: Juan Pablo Cañón Contreras
+#     Fecha: Mayo 2024
+#     Materia: Sistemas Operativos
+#     Tema: Taller de Evaluación de Rendimiento
+#     Fichero: script automatización ejecución por lotes 
+#	  Objetivo: Documentar el código y modificar los valores
+#				para la experimentación y salida de datos
+#****************************************************************
 
-$Nombre_Ejecutable = "MM_ejecutable";  # Asigna el nombre del ejecutable a la variable $Nombre_Ejecutable.
-@Size_Matriz = ("200","300");  # Define un array con dos tamaños de matriz: 200 y 300.
-@Num_Hilos = (1,2);  # Define un array con dos configuraciones de número de hilos: 1 y 2.
-$Repeticiones = 2;  # Establece el número de repeticiones de cada ejecución a 2.
+# Obtiene el directorio de trabajo actual y lo asigna a $Path
+my $Path = `pwd`;
 
-foreach $size (@Size_Matriz){  # Itera sobre cada tamaño de matriz en el array @Size_Matriz.
-	foreach $hilo (@Num_Hilos) {  # Anida un bucle para iterar sobre cada configuración de hilos en el array @Num_Hilos.
-		$file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";  # Construye el nombre del archivo para guardar los resultados de la ejecución.
-		for ($i=0; $i<$Repeticiones; $i++) {  # Bucle para repetir la ejecución la cantidad de veces especificada en $Repeticiones.
-#system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");  # Línea comentada que ejecutaría el comando para correr el ejecutable y redirigir la salida a $file.
-			printf("$Path/$Nombre_Ejecutable $size $hilo \n");  # Imprime el comando que se ejecutaría, útil para depuración.
-		}
-		close($file);  # Cierra el archivo después de escribir todas las salidas (aunque en este contexto no es necesario y no tiene efecto).
-	$p=$p+1;  # Incrementa una variable $p (no definida previamente) por cada combinación de tamaño de matriz y número de hilos.
-	}
+# Elimina cualquier salto de línea al final de la salida del comando 'pwd'
+chomp($Path);  
+
+# Define el nombre del ejecutable que se usará en las pruebas
+my $Nombre_Ejecutable = "Fuente_Evaluacion";
+
+# Define los tamaños de matrices para las pruebas en un arreglo
+my @Size_Matriz = ("200", "300");
+
+# Define los números de hilos que se usarán en las pruebas en otro arreglo
+my @Num_Hilos = (1, 2);
+
+# Establece el número de repeticiones que se realizarán para cada configuración
+my $Repeticiones = 10;
+
+# Ciclo anidado que itera sobre cada tamaño de matriz y número de hilos
+foreach my $size (@Size_Matriz) {
+    foreach my $hilo (@Num_Hilos) {
+
+		# Asigna el nombre concatenando la ruta, nombre del ejecutable, tamaño e hilos
+        my $file = "$Path/$Nombre_Ejecutable-$size-Hilos-$hilo.dat";
+
+		# Abrir el archivo y mostrar mensaje de error en caso de que no se pueda
+        open(my $fh, '>', $file) or die "No se pudo abrir el archivo '$file' $!";
+        
+		# Ciclo que itera por la cantidad de repeticiones
+        for (my $i = 0; $i < $Repeticiones; $i++) {
+            # Ejecutar linea de comando y sino se puede decir mensaje de error
+            open(my $cmd, "-|", "$Path/$Nombre_Ejecutable $size $hilo") or die "No se pudo ejecutar el comando: $!";
+            while (my $line = <$cmd>) {
+                print $fh $line;  # Imprime la linea en el archivo
+            }
+            close $cmd; #Finaliza el uso de la consola
+        }
+        
+        close $fh; #Cierra el archivo y finaliza el script
+    }
 }
-
 
